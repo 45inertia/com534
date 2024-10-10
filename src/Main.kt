332154e1d3762e0cf18ce3d64003343936cc5786
45inertia <5myero37@solent.ac.uk>
2024-10-10 13:50:18 +0100

    Initial setup of the CatApp before any of the exercises have been done

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main (args: Array<String>) {
+    val binnie = Cat("Binnie", 10, 10)
+    val clyde = Cat("Clyde", 5, 5)
+    binnie.walk()
+    clyde.walk()
+    println(binnie)
+    println(clyde)
+}
---
+++ src/cat.kt
@@ -0,0 +1,22 @@
+class Cat (nameIn: String, ageIn: Int, weightIn: Int) {
+
+    // Properties (attributes)
+    val name: String
+    var age: Int
+    var weight: Int
+
+    // Init block, for performing tasks on creation of the object
+    init {
+        name = nameIn
+        age = ageIn
+        weight = weightIn
+    }
+
+    fun walk() {
+        weight--
+    }
+
+    override fun toString() : String {
+        return "Name: $name Age: $age Weight: $weight"
+    }
+}
